16357ef0d5dde3013ee7a7ee0f9cbac594c10af1
Maryna Kosse <mkosse@kse.org.ua>
2025-02-17 11:04:26 +0200

    Implemented for double

--- GameDevHW1.cpp
+++ GameDevHW1.cpp
@@ -176,6 +176,159 @@ auto classify(float const value)
     else return "unknown";
 }
 
+// double
+
+bool is_finite(double const value)
+{
+    uint64_t bit_value(to_bytes(value));
+
+    if (!~(bit_value | 0b10000000'00001111'11111111'11111111'11111111'11111111'11111111'11111111)) {
+        if (bit_value >> 63 && (bit_value & 0b00000000'00001111'11111111'11111111'11111111'11111111'11111111'11111111)) {
+            return true;
+        }
+        else return false;
+    }
+    else return true;
+}
+
+bool is_inf(double const value)
+{
+    uint64_t bit_value(to_bytes(value));
+    bit_value <<= 1;
+
+    if (bit_value == 0b11111111'11100000'00000000'00000000'00000000'00000000'00000000'00000000) {
+
+        return true;
+    }
+    else return false;
+}
+
+bool is_pos_inf(double const value)
+{
+    uint64_t bit_value(to_bytes(value));
+
+    if (bit_value == 0b01111111'11110000'00000000'00000000'00000000'00000000'00000000'00000000) {
+
+        return true;
+    }
+    else return false;
+}
+
+bool is_neg_inf(double const value)
+{
+    uint64_t bit_value(to_bytes(value));
+
+    if (bit_value == 0b11111111'11110000'00000000'00000000'00000000'00000000'00000000'00000000) {
+
+        return true;
+    }
+    else return false;
+}
+
+bool is_zero(double const value)
+{
+    uint64_t bit_value(to_bytes(value));
+    bit_value <<= 1;
+
+    if (!(bit_value)) {
+
+        return true;
+    }
+    else return false;
+}
+
+bool is_pos_zero(double const value)
+{
+    uint64_t bit_value(to_bytes(value));
+
+    if (bit_value == 0b00000000'00000000'00000000'00000000'00000000'00000000'00000000'00000000) {
+
+        return true;
+    }
+    else return false;
+}
+
+bool is_neg_zero(double const value)
+{
+    uint64_t bit_value(to_bytes(value));
+
+    if (bit_value == 0b10000000'00000000'00000000'00000000'00000000'00000000'00000000'00000000) {
+
+        return true;
+    }
+    else return false;
+}
+
+bool is_nan(double const value)
+{
+    uint64_t bit_value(to_bytes(value));
+
+    if (bit_value >> 52 == 0b00000000'00000000'00000000'00000000'00000000'00000000'00000111'11111111) {
+
+        if (bit_value << 12) {
+            return true;
+        }
+        else return false;
+    }
+    else return false;
+}
+
+bool is_normal(double const value)
+{
+    uint64_t bit_value(to_bytes(value));
+
+    if (!(bit_value & 0b01111111'11110000'00000000'00000000'00000000'00000000'00000000'00000000)) {
+        return false;
+    }
+    if ((bit_value << 1) >> 53 == 0b00000000'00000000'00000000'00000000'00000000'00000000'00000111'11111111) {
+        if (bit_value << 12 && !(bit_value >> 63)) {
+            return false;
+        }
+        if (!(bit_value << 12)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool is_subnormal(double const value)
+{
+    uint64_t bit_value(to_bytes(value));
+    bit_value <<= 1;
+
+    if (bit_value >> 53 == 0b00000000'00000000'00000000'00000000'00000000'00000000'00000000'00000000 && bit_value) {
+        return true;
+    }
+    else return false;
+}
+
+bool is_signed(double const value)
+{
+    uint64_t bit_value(to_bytes(value));
+    return bit_value >> 63;
+}
+
+auto classify(double const value)
+{
+    if (is_inf(value)) {
+        return "Inf";
+    }
+    if (is_nan(value)) {
+        return "NaN";
+    }
+    if (is_normal(value)) {
+        return "normal";
+    }
+    if (is_subnormal(value)) {
+        return "subnormal";
+    }
+    if (is_zero(value)) {
+        return "zero";
+    }
+    else return "unknown";
+}
+
+
 int main()
 {
     //std::cout << std::bitset<32>(to_bytes(std::numeric_limits<double>::max()))
@@ -188,11 +341,11 @@ int main()
    
     //std::cout << typeid(~bit_value &= 0b01111111'10000000'00000000'00000000).name() << std::endl;
     std::cout << classify(1.0) << std::endl;
-    std::cout << classify(std::numeric_limits<float>::quiet_NaN()) << std::endl;
-    std::cout << classify(std::numeric_limits<float>::infinity()) << std::endl;
+    std::cout << classify(std::numeric_limits<double>::quiet_NaN()) << std::endl;
+    std::cout << classify(std::numeric_limits<double>::infinity()) << std::endl;
     std::cout << classify(0.0) << std::endl;
-    std::cout << classify(std::numeric_limits<float>::min() / 2.0) << std::endl;
-    std::cout << classify(std::numeric_limits<float>::max()) << std::endl;
+    std::cout << classify(std::numeric_limits<double>::min() / 2.0) << std::endl;
+    std::cout << classify(std::numeric_limits<double>::max()) << std::endl;
 
 }
 
